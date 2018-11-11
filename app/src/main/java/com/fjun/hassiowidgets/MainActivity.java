03325fea01b759f322f88d800678cb6cea984073
Johan Böhlin <johboh@gmail.com>
2018-11-11 10:19:28 +0100

    Add confirmation toast.

--- app/src/main/java/com/fjun/hassiowidgets/MainActivity.java
+++ app/src/main/java/com/fjun/hassiowidgets/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static com.fjun.hassiowidgets.Constants.KEY_PREFS_API_KEY;
 import static com.fjun.hassiowidgets.Constants.KEY_PREFS_HOST;
@@ -20,10 +21,13 @@ public class MainActivity extends AppCompatActivity {
         final SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         final EditText hostEditText = findViewById(R.id.host);
         final EditText apiKeyEditText = findViewById(R.id.api_key);
-        findViewById(R.id.save).setOnClickListener(view -> sharedPreferences.edit()
-                .putString(KEY_PREFS_HOST, hostEditText.getText().toString().trim())
-                .putString(KEY_PREFS_API_KEY, apiKeyEditText.getText().toString().trim())
-                .apply());
+        findViewById(R.id.save).setOnClickListener(view -> {
+            sharedPreferences.edit()
+                    .putString(KEY_PREFS_HOST, hostEditText.getText().toString().trim())
+                    .putString(KEY_PREFS_API_KEY, apiKeyEditText.getText().toString().trim())
+                    .apply();
+            Toast.makeText(this, R.string.toast_saved, Toast.LENGTH_SHORT).show();
+        });
 
         // Set current saved host and api key.
         hostEditText.setText(sharedPreferences.getString(KEY_PREFS_HOST, ""));
